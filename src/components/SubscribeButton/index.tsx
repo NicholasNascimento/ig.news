dc84d1fb18effbf4e6cf626b026a511a78f058ee
Nicholas <nicholas.an99@gmail.com>
2021-11-16 20:38:19 -0300

    Updating files and creating services's files

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
-import { useSession, signIn, signin } from 'next-auth/client';
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,12 +10,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const [session] = useSession();
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
-            signin('github')
+            signIn('github')
             return;
         }
 
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+        } catch (err) {
+            console.log(err);
+            alert(err.message);
+        }
     }
 
     return (
